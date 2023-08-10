8a499101210a4773f85d65861543c8b0359697f3
Do Jaewoo <herobine159@naver.com>
2023-08-10 18:28:47 +0900

    첫 번째 커밋
    그룹화된 파일들 생성, models/events,models/users 모델 정의

---
+++ models/users.py
@@ -0,0 +1,34 @@
+# 사용자 등록 및 로그인 처리를 위한 라우팅
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+# 사용자 모델 정의
+class user(BaseModel):
+    email: EmailStr                   # 사용자 이메일
+    password: str                     # 사용자 패스워드
+    events: Optional[List[Event]]     # 해당 사용자가 생성한 이벤트. 처음에는 비어있다.
+
+    # 샘플 데이터
+    class Config:
+        json_schema_extra={
+            "example":{
+                "email":"fastapi@packt.com",
+                "password":"strong!!!",
+                "events":[],
+            }
+        }
+
+class UserSignIn(BaseModel):
+    email:EmailStr
+    password:str
+
+    class Config:
+        json_schema_extra={
+            "example":{
+                "email":"fastapi@packt.com",
+                "password":"storng!!!",
+                "events":[],
+            }
+        }
