7763ccef84f221ead570079a4fa64e3835870f76
Do Jaewoo <herobine159@naver.com>
2023-08-11 17:14:46 +0900

    다섯 번째 커밋
    models/events.py 이벤트 모델 클래스 -> SQLModel의 테이블 클래스

--- models/events.py
+++ models/events.py
@@ -1,20 +1,20 @@
 # 이벤트 처리용 모델을 정의
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional,List
 
-from pydantic import BaseModel
-from typing import List
-
-# 이벤트 모델
-class Event(BaseModel):
-    id: int             # 자동 생성되는 고유 식별자
+# 이벤트 모델 -> SQLModel의 테이블 클래스 사용
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)             
     title: str          # 이벤트 타이틀
     image: str          # 이벤트 이미지 배너의 링크
     description: str    # 이벤트 설명
-    tags: List[str]     # 그룹화를 위한 이벤트 태그
+    tags: List[str] = Field(default=None, primary_key=True)           
     location: str       # 이벤트 위치
 
     # Config 서브 클래스 : 문서화할 때 샘플 데이터를 보여주기 위한 용도
     # 샘플 데이터를 정의하고, API를 통해 신규 이벤트를 생성할 때 참고할 수 있다.
     class Config:
+        arbitary_types_allowed=True
         json_schema_extra={
             "example":{
                 "title":"FastAPI Book Launch",
