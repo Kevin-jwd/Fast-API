1e94adb77c40b9999e13ee204d93f8dc6b71e062
Do Jaewoo <herobine159@naver.com>
2023-08-11 19:54:27 +0900

    열 번째 커밋
    이벤트 생성, 조회, 변경, 삭제 라우트 데이터베이스 모델과 연결

--- routes/events.py
+++ routes/events.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Request, status
 from database.connection import get_session
 from models.events import Event, EventUpdate
 from typing import List
+from sqlmodel import select
 
 event_router=APIRouter(
     tags=["Events"]
@@ -13,16 +14,19 @@ events=[]
 
 # 모든 이벤트를 추출하는 라우트
 @event_router.get("/", response_model=List[Event])
-async def retrieve_all_events() -> List[Event]:
+async def retrieve_all_events(session=Depends(get_session)) -> List[Event]:
+    statement=select(Event)
+    events=session.exec(statement).all()
     return events
 
 # 특정 ID의 이벤트 추출하는 라우트
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id:int) -> Event:
-    for event in events:
-        if event.id==id:
-            return event
-        # 해당 ID의 이벤트가 없을 때
+async def retrieve_event(id:int, session=Depends(get_session)) -> Event:
+    event=session.get(Event,id)
+    if event:
+        return event
+    
+    # 해당 ID의 이벤트가 없을 때
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist."
@@ -39,14 +43,15 @@ async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
     }
 
 # 데이터베이스에 있는 단일 이벤트 삭제
-@event_router.delete("/{id}")
-async def delete_event(id:int) -> dict:
-    for event in events:
-        if event.id==id:
-            events.remove(event)
-            return{
-                "message":"Event deleted successfully."
-            }
+@event_router.delete("/delete/{id}")
+async def delete_event(id:int, session=Depends(get_session)) -> dict:
+    event=session.get(Event,id)
+    if event:
+        session.delete(event)
+        session.commit()
+        return{
+            "message":"Event deleted successfully."
+        }
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist."
@@ -59,3 +64,22 @@ async def delete_all_events() -> dict:
     return {
         "message":"Events deleted successfully."
     }
+
+# UPDATE 라우트
+@event_router.put("/edit/{id}", response_model=Event)
+async def update_event(id:int, new_data:EventUpdate, session=Depends(get_session))->Event:
+    # 특정 이벤트를 추출해서 변경하는 코드
+    event=session.get(Event,id)
+    if event:
+        event_data=new_data.dict(exclude_unset=True)
+        for key, value in event_data.items():
+            setattr(event,key,value)
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+
+        return event
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist."
+    )
