bd2a63737b9a2c80f13c6e8dd22b38ba436a13f4
Do Jaewoo <herobine159@naver.com>
2023-08-13 22:16:08 +0900

    일곱 번쨰 커밋
    routes/events.py 에 라우트들 모두 변경

--- routes/events.py
+++ routes/events.py
@@ -1,53 +1,66 @@
 # 이벤트 처리용 모델을 정의
 
 from fastapi import APIRouter, Body, HTTPException, status
-from models.events import Event
+from models.events import Event, EventUpdate
 from typing import List
+from beanie import PydanticObjectId
+from database.connection import Database
 
 event_router=APIRouter(
     tags=["Events"]
 )
+event_database=Database(Event)
 
 events=[]
 
 # 모든 이벤트를 추출하는 라우트
 @event_router.get("/", response_model=List[Event])
 async def retrieve_all_events() -> List[Event]:
+    events=await event_database.get_all()
     return events
 
 # 특정 ID의 이벤트 추출하는 라우트
 @event_router.get("/{id}", response_model=Event)
 async def retrieve_event(id:int) -> Event:
-    for event in events:
-        if event.id==id:
-            return event
-        # 해당 ID의 이벤트가 없을 때
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event with supplied ID does not exist."
-    )
-    
+    event=await event_database.get_all()
+    if not event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND
+            detail="Event with supplied ID does not exist."
+        )
+    return event
+
 # 이벤트 생성 라우트
 @event_router.post("/new")
-async def create_event(body:Event=Body(...)) -> dict:
-    events.append(body)
+async def create_event(body:Event) -> dict:
+    await event_database.save(body)
     return{
         "message":"Event created successfully."
     }
 
+# UPDATE 라우트
+@event_router.put("/{id}", response_model=Event)
+async def update_event(id: PydanticObjectId, body:EventUpdate) -> Event:
+    updated_event=await event_database.update(id, body)
+    if not updated_event:
+        raise HTTPException(
+            status_code=status.404
+            detail="Event with supplied ID does not exist."
+        )
+    return updated_event
+
 # 데이터베이스에 있는 단일 이벤트 삭제
 @event_router.delete("/{id}")
-async def delete_event(id:int) -> dict:
-    for event in events:
-        if event.id==id:
-            events.remove(event)
-            return{
-                "message":"Event deleted successfully."
-            }
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event with supplied ID does not exist."
-    )
+async def delete_event(id:PydanticObjectId) -> dict:
+    event=await event_database.delete(id)
+    if not event:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Event with supplied ID does not exist."
+        )
+    return {
+        "message" : "Event deleted successfully."
+    }
 
 # 전체 이벤트 삭제
 @event_router.delete("/")
