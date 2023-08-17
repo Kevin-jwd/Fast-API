5dfad1eee624099ca59ab28b2e6778112d459ccc
Do Jaewoo <herobine159@naver.com>
2023-08-17 16:28:54 +0900

    열 두 번째 커밋
    routes/user.py 사용자 등록 라우트 수정 (사용자를 등록할 때 패스워드를 해싱), database/connection.py 에 SECRET_KEY 변수 추가, auth/jwt_handler.py 에 토큰 생성 함수와 토큰 검증 함수 추가

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -1,1 +1,54 @@
 # JWT 문자열을 인코딩, 디코딩하는 함수
+
+import time
+from datetime import datetime
+
+from fastapi import HTTPException, status
+# JWT 를 인코딩, 디코딩하는 라이브러리
+from jose import jwt, JWTError
+from database.connection import Settings
+
+settings=Settings()
+
+# 토큰 생성용 함수
+def create_access_token(user: str):
+    # 문자열 하나를 받아서 payload 딕셔너리에 연결
+    # payload 딕셔너리는 사용자명과 만료 시간을 포함하며 JWT가 디코딩될 때 반환됨.
+    payload={
+        "user":user,
+        "expires":time.time() + 3600        # 생성시점에서 한 시간 후
+    }
+
+    # encode() 메서드는 세 개의 인수를 받으며 payload를 암호화한다.
+    # 페이로드 : 값이 저장된 딕셔너리로, 인코딩할 대상
+    # 키 : 페이;로드를 사인하기 위한 키
+    # 알고리즘 : 페이로드를 사인 및 암호화하는 알고리즘, 기본값인 HS256 알고리즘을 가장 많이 사용
+    token=jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+    return token
+
+# 애플리케이션에 전달된 토큰을 검증하는 함수 (토큰을 문자열로 받는다.)
+def verify_access_token(token:str):
+    try:
+        data=jwt.decode(token,settings.SECRET_KEY, algorithms=["HS256"])
+        expire=data.get("expires")
+
+        # 토큰의 만료 시간이 존재하는지 여부 확인 (없으면 유요한 토큰이 존재하지 않는다고 판단)
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied."
+            )
+        
+        # 토큰이 유효한지 (만료 시간이 지나지 않았는지) 여부 확인
+        if datetime.utcnow() > datetime.utcfromtimestamp(expire):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Token expired!"
+            )
+        return data
+    
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid token."
+        )
--- routes/users.py
+++ routes/users.py
@@ -3,17 +3,20 @@
 from fastapi import APIRouter, HTTPException, status
 from models.users import User, UserSignIn
 from database.connection import Database
+from auth.hash_password import HashPassword
 
 user_router=APIRouter(
     tags=["User"]
 )
 
-user={}
 user_database=Database(User)
+hash_password=HashPassword()
 
 # 등록 라우트
 # 애플리케이션에 내장된 데이터베이스 사용
 @user_router.post("/signup")
+# 사용자 모델 (User)을 함수에 전달해 email 필드를 추출
+# User 클래스가 의존 라이브러리, 이를 sign_user_up()함수에 주입
 async def sign_new_user(user:User)->dict:
     user_exist=await User.find_one(User.email == user.email)
     if user_exist:
@@ -21,6 +24,9 @@ async def sign_new_user(user:User)->dict:
             status_code=status.HTTP_409_CONFLICT,
             detail="User with supplied username exists."
         )
+    # 패스워드를 해싱해서 데이터베이스에 저장
+    hashed_password=hash_password.create_hash(user.password)
+    user.password=hashed_password
     await user_database.save(user)
     return{
         "message":"User successfully registered!"
