c1100a3f0a741462e56c39501af7027b1be9973e
Do Jaewoo <herobine159@naver.com>
2023-08-22 00:17:14 +0900

    스물 세 번째 커밋
    로그인 라우트 테스트 작성

--- tests/test_login.py
+++ tests/test_login.py
@@ -24,3 +24,23 @@ async def test_sign_new_user(default_client: httpx.AsyncClient) -> None:
     # 응답을 비교해서 요청이 성공했는지 확인
     assert response.status_code == 200
     assert response.json() == test_response
+
+@pytest.mark.asyncio
+async def test_sign_user_in(default_client: httpx.AsyncClient) -> None:
+    payload={
+        "username" : "testuser@packt.com",
+        "password" : "testpassword",
+    }
+
+    # 요청 헤더와 응답을 정의
+    headers={
+        "accept" : "application/json",
+        "Content-Type" : "application/x-www-form-urlencoded"
+    }
+
+    # 요청에 대한 예상 응답 정의
+    response = await default_client.post("/user/signin", data=payload, headers=headers)
+
+    # 응답을 비교해서 요청이 성공했는지 확인
+    assert response.status_code == 200
+    assert response.json()["token_type"] == "Bearer"
